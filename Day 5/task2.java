7801f293f49981eae9bb90f9533f79502308696a
Abhilash Misra <abhilashmisra0001@gmail.com>
2018-06-22 13:26:43 +0530

    Added Day 5 codes

--- Day 5/task2.java
+++ Day 5/task2.java
@@ -1,13 +1,15 @@
 import java.io.*;
 import java.util.*;
-public class task1 {
+public class task2 {
 
 	public static void main(String[] args) throws IOException {
 		String s = "In this one we think we've reached the main verb when we get to raced but instead we are inside a reduced relative clause Reduced relative clauses let us say the speech given this morning instead of the speech that was given this morning Or inn this case the horse raced past the barn instead of the horse that was raced past the barn";
 		String words[] = s.split(" ");
-		Set<String> set = new HashSet<>();
+		Map<String, Integer> map = new HashMap<>();
 		for (String string : words)
-			set.add(string);
-		System.out.println(set);
+			map.put(string, 0);
+		for (String string : words) 
+			map.put(string, map.get(string)+1);
+		System.out.println(map);
 	}
 }
